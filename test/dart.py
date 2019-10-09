65af607b6cb94bf0e608a7e8877ab57ee7185d13
KeunYoung Park <superneo77@gmail.com>
2019-10-10 00:14:22 +0900

    feat: Add argparse to deal with the DART API key input

--- test/dart.py
+++ test/dart.py
@@ -1,11 +1,24 @@
-import requests
-
 # reference: http://dart.fss.or.kr/dsap001/guide.do
 
-URL = 'http://dart.fss.or.kr/api/search.json'
-params = {'auth': 'YOUR_API_KEY', 'page_set': 100}
-res = requests.get(URL, params=params)
+import argparse
+import requests
 
-print('status code: ' + str(res.status_code))
-print('json: ' + str(res.json()))
 
+def main(api_key):
+    URL = 'http://dart.fss.or.kr/api/search.json'
+    params = {'auth': api_key, 'page_set': 100}
+    res = requests.get(URL, params=params)
+
+    print('status code: ' + str(res.status_code))
+    print('json: ' + str(res.json()))
+
+
+if __name__ == '__main__':
+    print('[' + __file__ + '] main invoked.')
+
+    AP = argparse.ArgumentParser(description='arg parser')
+    AP.add_argument('-api_key', action='store', required=True,
+                    help='your API key for the DART service')
+    ARGS = AP.parse_args()
+
+    main(ARGS.api_key)
