13d694969ec31d6a24bce995abef4a79bb6c1005
KeunYoung Park <superneo77@gmail.com>
2019-10-14 00:16:25 +0900

    feat: Add a text example for company_condition

--- test/dart.py
+++ test/dart.py
@@ -1,6 +1,7 @@
 # reference: http://dart.fss.or.kr/dsap001/guide.do
 
 import argparse
+import json
 import requests
 
 
@@ -9,11 +10,12 @@ def company_search(params):
     http://dart.fss.or.kr/api/search.xml?auth=xxx     xml 응답
     http://dart.fss.or.kr/api/search.json?auth=xxx    json 응답
     '''
+    print('[company_search]')
     URL = 'http://dart.fss.or.kr/api/search.json'
     res = requests.get(URL, params=params)
 
     print('status code: ' + str(res.status_code))
-    print('json: ' + str(res.json()))
+    print('json:\n' + json.dumps(res.json(), indent=4, ensure_ascii=False))
 
 
 def company_condition(params):
@@ -21,12 +23,19 @@ def company_condition(params):
     http://dart.fss.or.kr/api/company.xml?auth=xxx&crp_cd=xxx    xml 응답
     http://dart.fss.or.kr/api/company.json?auth=xxx&crp_cd=xxx   json 응답
     '''
-    pass
+    print('[company_condition]')
+    URL = 'http://dart.fss.or.kr/api/company.json'
+    res = requests.get(URL, params=params)
+
+    print('status code: ' + str(res.status_code))
+    print('json:\n' + json.dumps(res.json(), indent=4, ensure_ascii=False))
 
 
 def main(api_key):
-    params = {'auth': api_key, 'page_set': 100}
-    company_search(params)
+    crp_cd = '005930'  # Samsung Electronics (just for example)
+    page_set = 100  # entities per page
+    company_search({'auth': api_key, 'page_set': page_set})
+    company_condition({'auth': api_key, 'crp_cd': crp_cd})
 
 
 if __name__ == '__main__':
